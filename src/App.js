829bc254bac03f75cfb2a7d5cf83ec5d8dae69ba
Ben Koehler <bkoehler85282@gmail.com>
2019-12-19 20:03:22 -0700

    Finished up login in form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import SignForm from "./components/SignUpForm";
+import LoginForm from "./components/Login";
 import Navigation from "./components/Navigation";
 import { Route } from "react-router-dom";
 import JokesCardDisplay from "./components/JokesCardDisplay";
@@ -12,6 +13,7 @@ function App() {
         <Navigation />
         <Route exact path="/" component={JokesCardDisplay} />
         <Route exact path="/signup" component={SignForm} />
+        <Route exact path="/login" component={LoginForm} />
       </div>
     </Router>
   );
