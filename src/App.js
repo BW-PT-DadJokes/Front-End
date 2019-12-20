755e8abbffc49539ad2ef1a729a8a0f47ef41763
Ben Koehler <bkoehler85282@gmail.com>
2019-12-19 19:22:06 -0700

    moved router to App.js instead of index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import SignForm from "./components/SignUpForm";
 import Navigation from "./components/Navigation";
 import { Route } from "react-router-dom";
 import JokesCardDisplay from "./components/JokesCardDisplay";
 function App() {
   return (
-    <div>
-      <Navigation />
-      <Route exact path="/" component={JokesCardDisplay} />
-      <Route exact path="/signup" component={SignForm} />
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Route exact path="/" component={JokesCardDisplay} />
+        <Route exact path="/signup" component={SignForm} />
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
@@ -14,12 +13,9 @@ import logger from "redux-logger";
 const store = createStore(applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    ,
-  </Router>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 
