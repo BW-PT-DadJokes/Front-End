544c77e9b629fa66a42e8381b45849dece436108
Ben Koehler <bkoehler85282@gmail.com>
2019-12-22 14:18:04 -0700

    added more navigation links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Nav, NavItem, NavbarBrand, NavLink } from "reactstrap";
-import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div>
       <Nav className="App">
         <NavbarBrand href="/">Dad Jokes</NavbarBrand>
+        <NavItem>
+          <NavLink href="https://bw-pt-dadjokes.github.io/Landing-Page/index.html">
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://bw-pt-dadjokes.github.io/Landing-Page/about-us.html">
+            About Us
+          </NavLink>
+        </NavItem>
         <NavItem>
           <NavLink href="/">Jokes</NavLink>
         </NavItem>
