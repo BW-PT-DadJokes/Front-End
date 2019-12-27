1422a729cb49b70441dd307e70afd80fa966c2c9
Ben Koehler <bkoehler85282@gmail.com>
2019-12-27 10:53:57 -0700

    setup to pass props correctly

--- src/components/jokes.js
+++ src/components/jokes.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Card, CardTitle, CardBody } from "reactstrap";
-
 const JokeCard = props => {
   console.log(props.user_id);
   console.log(props.question);
   console.log(props.punchline);
 
-  return <div key={props.user_id}></div>;
+  return (
+    <div key={props.question}>
+      <Card>
+        <CardTitle>Questoin: {props.question}</CardTitle>
+        <CardBody>PunchLine: {props.punchline}</CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default JokeCard;
